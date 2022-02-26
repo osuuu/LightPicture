d39f6525e94cf3fed531625dedf1d82926baa553
初涧 <admin@osuu.net>
2022-02-26 15:15:09 +0800

    :bug:修复部分显示问题

--- app/common.php
+++ app/common.php
@@ -8,9 +8,9 @@
 
 
 // 程序版本
-define("VERSION", "1.2.0");
+define("VERSION", "1.2.1");
 // 版本号
-define("RELRAASE_TIME", "20220219");
+define("RELRAASE_TIME", "20220226");
 // 本地存储根目录
 define("FOLDER", "LightPicture/");
 // TOKEN KEY
